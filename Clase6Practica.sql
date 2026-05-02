ae7c3cb5446ff0e1a172278b21e4415317e254be
Joseph Mansilla <jomansilla@frba.utn.edu.ar>
2026-05-02 19:13:01 -0300

    dos casos del punto 8 terminados

--- Clase6Practica.sql
+++ Clase6Practica.sql
@@ -108,22 +108,54 @@ WHERE customer_num NOT IN (SELECT o.customer_num FROM items i JOIN orders o ON (
 -- 8) Obtener el número, nombre y apellido de los clientes que hayan comprado TODOS los
 --	productos del fabricante ‘HSK’.
 
--- fabrica 10 - vende 10 -> se vendió todo (comparaciones)
--- hay algun producto que no le compre a HSK -> no se vendio todo (op. de conj)
+-- CASO OPERACIONES
+-- fabrica 10 - vende 10 -> se vendió todo
 
 SELECT customer_num AS numero_cliente, fname+' '+lname AS nombre_cliente 
 FROM customer c 
-WHERE NOT EXISTS (SELECT * FROM products W)
+WHERE (
+	SELECT COUNT(DISTINCT stock_num) FROM products 
+	WHERE manu_code = 'HSK'
+	)
+	=
+	(SELECT COUNT(DISTINCT stock_num) FROM orders o
+	JOIN items i ON (o.order_num = i.order_num)
+	WHERE o.customer_num = c.customer_num AND i.manu_code='HSK'
+	)
+ORDER BY customer_num ASC;
 
-SELECT manu_code, COUNT(*) FROM products
-GROUP BY manu_code
-ORDER BY manu_code ASC;
+-- CASO OPERACIONES DE CONJUNTOS 
+-- hay algun producto que no le compre a HSK -> no se vendio todo
 
-SELECT DISTINCT manu_code, COUNT(*), o.customer_num FROM items i JOIN orders o ON (i.order_num = o.order_num)
-GROUP BY manu_code, o.customer_num
-ORDER BY manu_code ASC;
+SELECT customer_num AS numero_cliente, fname+' '+lname AS nombre_cliente 
+FROM customer c 
+WHERE NOT EXISTS (
+	SELECT stock_num FROM products 
+	WHERE manu_code = 'HSK'
+
+	EXCEPT
+
+	SELECT stock_num FROM orders o
+	JOIN items i ON (o.order_num = i.order_num)
+	WHERE c.customer_num = o.customer_num AND i.manu_code='HSK'
+)
+ORDER BY customer_num;
 
 
+-- ACÁ se visualiza cuales son los productos de cada manufacturador
+SELECT p.stock_num, pt.description, p.manu_code FROM products p 
+	JOIN product_types pt ON (pt.stock_num = p.stock_num)
+	-- WHERE p.manu_code = 'ANZ'
+	ORDER BY p.manu_code ASC;
+
+
+-- Acá se visualizan los productos que compró cada cliente respecto el manufacturador
+SELECT i.stock_num, pt.description, i.manu_code, c.customer_num FROM orders o 
+	JOIN items i ON (o.order_num = i.order_num)
+	JOIN customer c ON (c.customer_num = o.customer_num)
+	JOIN product_types pt ON (i.stock_num = pt.stock_num)
+	WHERE c.customer_num = o.customer_num -- AND p.manu_code = 'ANZ'
+	ORDER BY manu_code ASC, c.customer_num ASC;
 
 
 -- 9) Reescribir la siguiente consulta utilizando el operador UNION:

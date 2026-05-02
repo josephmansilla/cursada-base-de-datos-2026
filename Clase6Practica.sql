3819256f61c555cd38c753a4b750c125199d25a7
Joseph Mansilla <jomansilla@frba.utn.edu.ar>
2026-05-02 19:14:46 -0300

    dos casos del punto 8 terminados

--- Clase6Practica.sql
+++ Clase6Practica.sql
@@ -105,8 +105,7 @@ WHERE customer_num NOT IN (SELECT o.customer_num FROM items i JOIN orders o ON (
 					WHERE manu_code LIKE 'HSK');
 				*/
 
--- 8) Obtener el número, nombre y apellido de los clientes que hayan comprado TODOS los
---	productos del fabricante ‘HSK’.
+-- 8) Obtener el número, nombre y apellido de los clientes que hayan comprado TODOS productos del fabricante ‘HSK’.
 
 -- CASO OPERACIONES
 -- fabrica 10 - vende 10 -> se vendió todo
@@ -119,7 +118,7 @@ WHERE (
 	)
 	=
 	(SELECT COUNT(DISTINCT stock_num) FROM orders o
-	JOIN items i ON (o.order_num = i.order_num)
+		JOIN items i ON (o.order_num = i.order_num)
 	WHERE o.customer_num = c.customer_num AND i.manu_code='HSK'
 	)
 ORDER BY customer_num ASC;
@@ -136,24 +135,26 @@ WHERE NOT EXISTS (
 	EXCEPT
 
 	SELECT stock_num FROM orders o
-	JOIN items i ON (o.order_num = i.order_num)
+		JOIN items i ON (o.order_num = i.order_num)
 	WHERE c.customer_num = o.customer_num AND i.manu_code='HSK'
 )
 ORDER BY customer_num;
 
 
 -- ACÁ se visualiza cuales son los productos de cada manufacturador
-SELECT p.stock_num, pt.description, p.manu_code FROM products p 
-	JOIN product_types pt ON (pt.stock_num = p.stock_num)
+SELECT p.stock_num, pt.description, p.manu_code 
+	FROM products p 
+		JOIN product_types pt ON (pt.stock_num = p.stock_num)
 	-- WHERE p.manu_code = 'ANZ'
 	ORDER BY p.manu_code ASC;
 
 
 -- Acá se visualizan los productos que compró cada cliente respecto el manufacturador
-SELECT i.stock_num, pt.description, i.manu_code, c.customer_num FROM orders o 
-	JOIN items i ON (o.order_num = i.order_num)
-	JOIN customer c ON (c.customer_num = o.customer_num)
-	JOIN product_types pt ON (i.stock_num = pt.stock_num)
+SELECT i.stock_num, pt.description, i.manu_code, c.customer_num 
+	FROM orders o 
+		JOIN items i ON (o.order_num = i.order_num)
+		JOIN customer c ON (c.customer_num = o.customer_num)
+		JOIN product_types pt ON (i.stock_num = pt.stock_num)
 	WHERE c.customer_num = o.customer_num -- AND p.manu_code = 'ANZ'
 	ORDER BY manu_code ASC, c.customer_num ASC;
 

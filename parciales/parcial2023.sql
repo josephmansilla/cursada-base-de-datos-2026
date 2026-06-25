845fafc900c226544241ca04c7fdd58e4c365324
Joseph Mansilla <jomansilla@frba.utn.edu.ar>
2026-06-25 16:00:12 -0300

    parcial 2023 terminado

--- parciales/parcial2023.sql
+++ parciales/parcial2023.sql
@@ -19,11 +19,78 @@ WHERE ranking <= 2
 ORDER BY estado, monto_total_comprado DESC;
 -- PUNTO 4 - PROCEDURE
 
---CREATE PROCEDURE borrarprod
---BEGIN
+CREATE TABLE audit_products(
+	id_audit BIGINT PRIMARY KEY IDENTITY(1,1),
+	stock_num SMALLINT,
+	manu_code CHAR(3),
+	message VARCHAR(255)
+);
+
+CREATE TABLE productos_deprecados(
+	stock_num SMALLINT,
+	manu_code CHAR(3)
+	CONSTRAINT PK_deprecados PRIMARY KEY (stock_num, manu_code)
+);
+
+ALTER TABLE products
+ADD status CHAR;
+
+GO
+CREATE PROCEDURE borrar_deprecados
+AS BEGIN 
 	
---END;
---GO
+	DECLARE cur_borrado CURSOR FOR --> es necesario usar un cursor? para mi !,,,,,, posiblemente... pero no se como se podria hacer la parte de auditoria
+	SELECT stock_num, manu_code FROM productos_deprecados;
+
+	DECLARE @stock_num SMALLINT; DECLARE @manu_code CHAR(3);
+	 
+	OPEN cur_borrado;
+	
+	FETCH FROM cur_borrado
+	INTO @stock_num, @manu_code;
+
+	WHILE (@@FETCH_STATUS = 0)
+	BEGIN
+	BEGIN TRANSACTION
+	BEGIN TRY 
+		
+		IF EXISTS (SELECT * FROM products WHERE status = 'D' AND @stock_num = stock_num AND @manu_code = manu_code)
+			THROW 50010, 'PRODUCTO YA DEPRECADO', 16; --> en caso de ya estar deprecado, tiramos error y nos vamos al catch
+		IF NOT EXISTS (SELECT * FROM products WHERE @stock_num = stock_num AND @manu_code = manu_code)
+			THROW 50011, 'PRODUCTO NO EXISTE', 16; --> manejo de error por ahi de más...
+
+		UPDATE products
+		SET status = 'D' 
+		WHERE stock_num = @stock_num AND @manu_code = manu_code;
+
+		IF EXISTS (SELECT order_num, item_num
+				   FROM products p JOIN items it ON (p.manu_code = it.manu_code AND p.stock_num = it.stock_num)
+				   WHERE p.stock_num = @stock_num AND @manu_code = p.manu_code)
+		BEGIN
+			INSERT INTO audit_products (stock_num, manu_code, message)
+			VALUES (@stock_num, @manu_code, 'DEPRECADO CON VENTAS'); --> insertamos si tiene ventas
+		END;
+		ELSE BEGIN
+			INSERT INTO audit_products (stock_num, manu_code, message)
+			VALUES (@stock_num, @manu_code, 'DEPRECADO'); --> insertamos si no tiene ventas
+		END;
+
+		COMMIT TRANSACTION;
+	END TRY 
+	BEGIN CATCH
+		ROLLBACK TRANSACTION --> deshacemos
+		INSERT INTO audit_products (stock_num, manu_code, message)
+		VALUES (@stock_num, @manu_code, ERROR_MESSAGE()) --> insertamos el error
+	END CATCH
+
+		FETCH NEXT FROM cur_borrado
+		INTO @stock_num, @manu_code; 
+	END;
+
+	CLOSE cur_borrado;
+	DEALLOCATE cur_borrado;
+END;
+GO
 
 
 -- PUNTO 5 - TRIGGER 
